251a2a811a8c4f143c2491bc7b96b29b378e9f02
Andreas <andreas.landerer@vontobel.com>
2023-10-11 16:58:09 +0200

    Added VM instance with a static IP and outputs

--- main.tf
+++ main.tf
@@ -44,3 +44,23 @@ resource "google_compute_network" "vpc_network" {
         google_project_service.billing_api
     ]
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name = "terraform-instance"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
