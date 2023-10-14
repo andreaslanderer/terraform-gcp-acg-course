f4bac4dab0d88eed17037928e0defb7f045194b1
Andreas <andreas.landerer@vontobel.com>
2023-10-14 11:52:35 +0200

    Started using module to create the VPC network

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
-provider "google" {
-    project = var.PROJECT_ID
-    region = "europe-west6"
-    zone = "europe-west6-a"
+module "network" {
+  source = "terraform-google-modules/network/google"
+  version = "7.4.0"
+  network_name = "acg-terraform-vpc"
+  project_id = var.PROJECT_ID
+  subnets = []
+}
+
+variable "PROJECT_ID" {
+    description = "The unique ID of your GCP project"
+    default = "not_provided"
 }
 
 terraform {
     backend "gcs" {
         prefix = "tfstate"
     }
-}
-
-variable "PROJECT_ID" {
-    description = "The unique ID of your GCP project"
-    default = "not_provided"
 }
 
 resource "google_project_service" "resource_manager_api" {
@@ -33,34 +35,4 @@ resource "google_project_service" "iam_api" {
 resource "google_project_service" "billing_api" {
   project = var.PROJECT_ID
   service = "cloudbilling.googleapis.com"
-}
-
-resource "google_compute_network" "vpc_network" {
-    name = "acg-terraform-vpc"
-    depends_on = [ 
-        google_project_service.resource_manager_api,
-        google_project_service.compute_api,
-        google_project_service.iam_api,
-        google_project_service.billing_api
-    ]
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name = "terraform-instance"
-  machine_type = "f1-micro"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-12"
-    }
-  }
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-      
-    }
-  }
-}
-
-resource "google_compute_address" "vm_static_ip" {
-  name = "terraform-static-ip"
 }
